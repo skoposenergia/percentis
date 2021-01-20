7d0852d5a8de223a3304b2ddaadd366c066cb232
mazucanti <daniel.mazucanti.ismart@gmail.com>
2021-01-20 13:05:06 -0300

    refatorações e testes de parâmetros

--- gpl.py
+++ gpl.py
@@ -13,8 +13,7 @@ id_models = []
 token = authenticatePluvia(user, psswd)
 
 precipitationDataSources = ["ECMWF_ENS", "ECMWF_ENS_EXT"]
-specs = ["", ""]
-forecastModels = ['IA+SMAP']
+forecastModels = ['IA+SMAP', 'IA+SMAP']
 
 for precipitationDataSource in precipitationDataSources:
     id_maps.append(getIdOfPrecipitationDataSource(precipitationDataSource))
@@ -22,22 +21,22 @@ for precipitationDataSource in precipitationDataSources:
 for forecastModel in forecastModels:
     id_models.append(getIdOfForecastModel(forecastModel))
 
-curr_day = dt.datetime.today()
+current_day = dt.datetime.today()
 
-forecastdate = curr_day.strftime("%d/%m/%Y")
-form_dir = curr_day.strftime("%Y-%m-%d")
+forecastdate = current_day.strftime("%d/%m/%Y")
+format_directory = current_day.strftime("%Y-%m-%d")
 
-dir_download = Path('entradas')
-if not (dir_download.exists()):
-    Path.mkdir(dir_download)
+directoryOfDownload = Path('Arquivos/%s' % format_directory)
+if not (directoryOfDownload.exists()):
+    Path.mkdir(directoryOfDownload)
 
 forecasts = getForecasts(forecastdate, id_maps,
-                         id_models, '', '', [curr_day.year], [])
+                         id_models, '', '', [current_day.year], [])
 
 for forecast in forecasts:
     # downloadForecast(forecast['prevsId'], dir_download,
     #                  forecast['nome'] + ' - ' + forecast['membro'] + ' - Prevs.zip')
-    downloadForecast(forecast['enaId'], dir_download,
+    downloadForecast(forecast['enaId'], directoryOfDownload,
                      forecast['nome'] + ' - ' + forecast['membro'] + '- ENA.zip')
     # downloadForecast(forecast['vnaId'], dir_download, forecast['nome'] + ' - ' + forecast['membro'] + '- VNA.csv')
     # downloadForecast(forecast['strId'], dir_download, forecast['nome'] + ' - ' + forecast['membro'] + '- STR.zip')
