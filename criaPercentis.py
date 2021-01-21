e6148607fd4d019398072b7fc65e3581e823160e
mazucanti <daniel.mazucanti.ismart@gmail.com>
2021-01-21 11:24:16 -0300

    Remoção de arquivos desnecessários para o procedimento

--- criaPercentis.py
+++ criaPercentis.py
@@ -1,9 +1,14 @@
 import pandas as pd
 from zipfile import ZipFile
+from pathlib import Path
 
 
 def zip_extraction(filePath):
     zips = [file for file in filePath.glob("**/*") if file.suffix == ".zip"]
     with ZipFile(file) as zp:
-        zp.extractall()
+        zp.extractall("ENA Files")
 
+def remove_enaprevs():
+    enaprevs = [item for item in Path("ENA Files").glob("**/*") if "ENAPREVS" in file.name]
+    for item in enaprevs:
+        item.unlink()
