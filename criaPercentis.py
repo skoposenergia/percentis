b178eb2f98658da83216f8eb25454b359d283c4e
mazucanti <daniel.mazucanti.ismart@gmail.com>
2021-02-04 12:34:49 -0300

    Função de importação

--- criaPercentis.py
+++ criaPercentis.py
@@ -4,27 +4,32 @@ from pathlib import Path
 import datetime as dt
 from gpl import download_base_files
 
-def ecmwf_ens_extraction(filePath):
-    zips = [file for file in filePath.glob("**/*") if not ("EXT" in file.stem)]
-    with ZipFile(file) as zp:
-        zp.extractall("ENA Files")
+def ecmwf_ens_extraction():
+    zips = [item for item in Path("Downloads/").glob("**/*") if not ("EXT" in item.stem) and item.suffix == ".zip"]
+    for item in zips:
+        with ZipFile(item) as zp:
+            zp.extractall("ENA Files")
 
 def remove_enaprevs():
     enaprevs = [item for item in Path("ENA Files").glob("**/*") if "ENAPREVS" in item.name]
     for item in enaprevs:
         item.unlink()
 
-def remove_ensemble():
+def remove_ensembles():
     ensembles = [item for item in Path('Downloads').glob("**/*") if "ENSEMBLE" in item.name]
     for item in ensembles:
         item.unlink()
 
 def import_ena(filePath):
-    pass
+    df = pd.read_csv(filePath,sep=";",decimal=",",skiprows=59)
+    return df
 
 def get_zips():
     dateStr = input("Digite o dia dos arquivos (dd/mm/yyyy)\n")
     targetDate = dt.datetime.strptime(dateStr, "%d/%m/%Y")
     download_base_files(targetDate)
 
-get_zips()
+
+def filter_subs(df):
+    pass
+
