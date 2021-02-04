3466a9513f39c9d7d0de6f4e4e46caa9e2587204
mazucanti <daniel.mazucanti.ismart@gmail.com>
2021-02-04 11:32:02 -0300

    função de download e remoção de arquivos ensemble prontas

--- criaPercentis.py
+++ criaPercentis.py
@@ -1,14 +1,30 @@
 import pandas as pd
 from zipfile import ZipFile
 from pathlib import Path
+import datetime as dt
+from gpl import download_base_files
 
-
-def zip_extraction(filePath):
-    zips = [file for file in filePath.glob("**/*") if file.suffix == ".zip"]
+def ecmwf_ens_extraction(filePath):
+    zips = [file for file in filePath.glob("**/*") if not ("EXT" in file.stem)]
     with ZipFile(file) as zp:
         zp.extractall("ENA Files")
 
 def remove_enaprevs():
-    enaprevs = [item for item in Path("ENA Files").glob("**/*") if "ENAPREVS" in file.name]
+    enaprevs = [item for item in Path("ENA Files").glob("**/*") if "ENAPREVS" in item.name]
     for item in enaprevs:
         item.unlink()
+
+def remove_ensemble():
+    ensembles = [item for item in Path('Downloads').glob("**/*") if "ENSEMBLE" in item.name]
+    for item in ensembles:
+        item.unlink()
+
+def import_ena(filePath):
+    pass
+
+def get_zips():
+    dateStr = input("Digite o dia dos arquivos (dd/mm/yyyy)\n")
+    targetDate = dt.datetime.strptime(dateStr, "%d/%m/%Y")
+    download_base_files(targetDate)
+
+get_zips()
